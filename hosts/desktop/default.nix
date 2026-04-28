b28663c752de7fd337a67c09405cff920266ef34
Pavel Grigoriev <grigpavel21@gmail.com>
2026-04-28 19:40:05 +0200

    clean desktop

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -29,79 +29,18 @@
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
-    git
-    vim
-    curl
-    wget
-    ripgrep
-    fd
-    unzip
-    zip
-    tree
-    eza
-    fzf
-
     fastfetch
     htop
     btop
 
     kitty
     alacritty
-    firefox
-    pavucontrol
-    xwayland-satellite
-    fuzzel
-    swaylock
-    swayidle
-    waybar
-    mako
   ];
 
   programs.niri.enable = true;
 
-  services.greetd = {
-    enable = true;
-    settings.default_session = {
-      command = "${config.programs.niri.package}/bin/niri-session";
-      user = "daze";
-    };
-  };
-
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  security.polkit.enable = true;
-  services.gnome.gnome-keyring.enable = true;
-  security.pam.services.swaylock = { };
-
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
-    config.niri = {
-      "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
-    };
-  };
-
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
   services.openssh.enable = true;
-  networking.firewall.enable = true;
-
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-color-emoji
-    dejavu_fonts
-    liberation_ttf
-    jetbrains-mono
-  ];
+  networking.firewall.enable = false;
 
   system.stateVersion = "25.11";
 }
